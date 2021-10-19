4ba7f36f50935599dec3161fc92817e18be4e6e6
jona7150 <jturpin@esri.com>
2021-10-19 12:21:56 -0700

    Updated Pseudo i18n - @jona7150

---
+++ src/nls/bg/resources.js
@@ -0,0 +1,278 @@
+define({
+  "appInit": "й_Initializing application____________________________й...",
+  "appFailed": "й_Failed to load the app________________________й.",
+  "noAuth": "й_Your account is not licensed to use Configurable Apps that are not public. Please ask your organization administrator to assign you a user type that includes Essential Apps or an add-on Essential Apps license__________________________________________________________________________________________________________й.",
+  "notLicensed": "й_Not Licensed_____________й",
+  "error": "й_Error___________й",
+  "originError": {
+    "message": "й_The application you are trying to access does not originate from the URL you provided____________________________________________й",
+    "close": "й_Close___________й",
+    "options": "й_Advanced options_________________й",
+    "linkMessage": "й_Proceed to_____________________й"
+  },
+  "analyticsOptIn": "й_Opt in_____________й",
+  "badges": {
+    "authoritative": "й_Authoritative______________й",
+    "deleted": "й_Deleted_______________й",
+    "deprecated": "й_Deprecated_____________________й",
+    "livingAtlas": "й_Living Atlas_____________й",
+    "marketplace": "й_Marketplace____________й",
+    "openData": "й_Open Data___________________й",
+    "premium": "й_Premium_______________й",
+    "subscriber": "й_Subscriber_____________________й",
+    "tooltips": {
+      "altAuthoritative": "й_Authoritative: Recommended by______________________________й",
+      "authoritative": "й_Authoritative: Recommended by your organization_________________________й",
+      "deprecated": "й_Deprecated: Not recommended for use___________________й",
+      "livingAtlas": "й_Living Atlas: Esri-curated content__________________й",
+      "marketplace": "й_Listed on the ArcGIS Marketplace_________________й",
+      "openData": "й_Available for public use_________________________й",
+      "premiumAnon": "й_Premium: Requires signing in with an ArcGIS Online subscription and consumes credits___________________________________________й",
+      "subscriberAnon": "й_Subscriber: Requires signing in with an ArcGIS Online subscription__________________________________й",
+      "premiumPublic": "й_Premium: Requires an ArcGIS Online subscription and consumes credits___________________________________й",
+      "subscriberPublic": "й_Subscriber: Requires an ArcGIS Online subscription__________________________й",
+      "premiumOrg": "й_Premium: Exclusive content for subscribers. Consumes credits________________________________й.",
+      "subscriberOrg": "й_Subscriber: Exclusive content for subscribers________________________й"
+    }
+  },
+  "buttons": {
+    "clear": "й_Clear___________й"
+  },
+  "dateSelection": {
+    "today": "й_Today___________й",
+    "yesterday": "й_Yesterday___________________й",
+    "last7Days": "й_Last 7 Days____________й",
+    "last30Days": "й_Last 30 Days_____________й",
+    "custom": "й_Custom Range________________й...",
+    "from": "й_Start date_____________________й",
+    "to": "й_End date_________________й"
+  },
+  "dropdowns": {
+    "clear": "й_Clear___________й",
+    "clearAll": "й_Clear All___________________й",
+    "contentViews": {
+      "grid": "й_Grid_________й",
+      "list": "й_List_________й",
+      "table": "й_Table___________й",
+      "view": "й_View_________й"
+    },
+    "sorting": {
+      "sort": "й_Sort_________й",
+      "sortBy": "й_Sort by_______________й",
+      "sortDir": "й_Sort direction_______________й",
+      "relevance": "й_Relevance___________________й",
+      "title": "й_Title___________й",
+      "owner": "й_Owner___________й",
+      "created": "й_Date Created_____________й",
+      "modified": "й_Date Modified______________й",
+      "numviews": "й_View Count_____________________й",
+      "avgrating": "й_Rating_____________й",
+      "ascending": {
+        "relevance": "й_Least to most relevant_______________________й",
+        "title": "й_Alphabetical_____________й",
+        "owner": "й_Alphabetical_____________й",
+        "created": "й_Least recent_____________й",
+        "modified": "й_Least recent_____________й",
+        "numviews": "й_Least to most______________й",
+        "avgrating": "й_Lowest to highest rated________________________й"
+      },
+      "descending": {
+        "relevance": "й_Most to least relevant_______________________й",
+        "title": "й_Reverse-alphabetical_____________________й",
+        "owner": "й_Reverse-alphabetical_____________________й",
+        "created": "й_Most recent____________й",
+        "modified": "й_Most recent____________й",
+        "numviews": "й_Most to least______________й",
+        "avgrating": "й_Highest to lowest rated________________________й"
+      }
+    }
+  },
+  "filters": {
+    "itemType": {
+      "itemType": "й_Item Type___________________й",
+      "filters": {
+        "maps": "й_Maps_________й",
+        "webMaps": "й_Web Maps_________________й",
+        "mapFiles": "й_Map Files___________________й",
+        "layers": "й_Layers_____________й",
+        "featureLayers": "й_Feature Layers_______________й",
+        "tileLayers": "й_Tile Layers____________й",
+        "mapImageLayers": "й_Map Image Layers_________________й",
+        "imageryLayers": "й_Imagery Layers_______________й",
+        "sceneLayers": "й_Scene Layers_____________й",
+        "tables": "й_Tables_____________й",
+        "layerFiles": "й_Layer Files____________й",
+        "scenes": "й_Scenes_____________й",
+        "apps": "й_Apps_________й",
+        "webApps": "й_Web Apps_________________й",
+        "mobileApps": "й_Mobile Apps____________й",
+        "desktopApps": "й_Desktop Apps_____________й",
+        "tools": "й_Tools___________й",
+        "locators": "й_Locators_________________й",
+        "geodatabaseAccess": "й_Geodatabase Access___________________й",
+        "geometricOperations": "й_Geometric Operations_____________________й",
+        "geoprocessingTasks": "й_Geoprocessing Tasks____________________й",
+        "networkAnalysis": "й_Network Analysis_________________й",
+        "files": "й_Files___________й",
+        "documents": "й_Documents___________________й",
+        "images": "й_Images_____________й",
+        "pdfs": "й_PDFs_________й",
+        "webTools": "й_Web Tools___________________й",
+        "notebooks": "й_Notebooks___________________й"
+      }
+    },
+    "date": {
+      "dateCreated": "й_Date Created_____________й",
+      "dateModified": "й_Date Modified______________й"
+    },
+    "shared": {
+      "shared": "й_Shared_____________й",
+      "options": {
+        "public": "й_With Everyone (public)_______________________й",
+        "org": "й_With Organization__________________й",
+        "shared": "й_With Groups____________й",
+        "private": "й_With No One____________й"
+      }
+    },
+    "status": {
+      "status": "й_Status_____________й",
+      "options": {
+        "authoritative": "й_Authoritative______________й",
+        "deprecated": "й_Deprecated_____________________й",
+        "premium": "й_Premium_______________й"
+      }
+    },
+    "tags": {
+      "tags": "й_Tags_________й",
+      "filterTags": "й_Filter tags____________й",
+      "noTags": "й_No tags were found matching your search_____________________й."
+    }
+  },
+  "gallery": {
+    "signIn": "й_Sign In_______________й",
+    "signOut": "й_Sign Out_________________й",
+    "header": {
+      "browse": "й_Browse_____________й",
+      "search": "й_Search_____________й"
+    },
+    "resultCount": "й_Items_____________й:",
+    "searchPlaceholders": {
+      "generic": "й_Enter search terms___________________й"
+    },
+    "filterChips": {
+      "mapArea": "й_Within Map Area________________й",
+      "type": "й_Type_________й",
+      "dateModified": "й_Modified_________________й",
+      "dateCreated": "й_Created_______________й",
+      "access": "й_Shared_____________й",
+      "group": "й_Group___________й",
+      "folder": "й_Folder_____________й",
+      "status": "й_Status_____________й",
+      "clearAll": "й_Clear All___________________й",
+      "category": "й_Category_________________й",
+      "region": "й_Region_____________й",
+      "tagged": "й_Tagged_____________й"
+    },
+    "itemDetails": {
+      "addToMap": "й_Add to Map_____________________й",
+      "removeFromMap": "й_Remove from Map________________й",
+      "by": "й_by_____й",
+      "lastModified": "й_Updated_______________й",
+      "noSnippet": "й_A brief summary of the item is not available________________________й.",
+      "details": "й_Description____________й",
+      "termsOfUse": "й_Terms of Use_____________й",
+      "attribution": "й_Credits (Attribution)______________________й",
+      "noTermsOfUse": "й_No special restrictions or limitations on using the item’s content have been provided____________________________________________й.",
+      "noAttribution": "й_No acknowledgements_____________________й.",
+      "noDescription": "й_An in-depth description of the item is not available____________________________й.",
+      "views": "й_View Count_____________________й",
+      "created": "й_Created_______________й",
+      "sharedWith": "й_Shared with____________й",
+      "shared": {
+        "public": "й_Everyone (public)__________________й",
+        "org": "й_Organization_____________й",
+        "shared": "й_The item is not shared________________________й.",
+        "private": "й_The item is not shared________________________й."
+      },
+      "viewUser": "й_View user profile__________________й",
+      "viewOrg": "й_Visit organization___________________й",
+      "addToFavorites": "й_Add to Favorites_________________й",
+      "removeFromFavorites": "й_Remove from Favorites______________________й",
+      "managedBy": "й_Managed by____________й:"
+    },
+    "results": {
+      "loadingItems": "й_Loading items________________й..",
+      "requestError": "й_There was an error with the request___________________й.",
+      "noItemsFound": "й_No items found that meet your criteria. Try clearing some filters to show more items____________________________________________й.",
+      "empty": "й_Enter some terms above to begin your search_______________________й."
+    },
+    "search": "й_Search_____________й",
+    "close": "й_Close___________й",
+    "filterPane": {
+      "filter": "й_Filter_____________й",
+      "categories": "й_Categories_____________________й",
+      "groupCategories": "й_Group Categories_________________й"
+    },
+    "viewDetails": "й_View item details__________________й",
+    "back": "й_Back_________й",
+    "compact": "й_Table___________й",
+    "compactView": "й_Compact view_____________й",
+    "list": "й_List_________й",
+    "listView": "й_List view___________________й",
+    "showing": "й_Showing_______________й",
+    "viewResults": "й_View results_____________й"
+  },
+  "itemCards": {
+    "remove": "й_Remove_____________й",
+    "add": "й_Add_______й",
+    "by": "й_by_____й",
+    "actions": "й_Actions_______________й",
+    "viewOrg": "й_Visit organization___________________й",
+    "viewProfile": "й_View user profile__________________й",
+    "viewItem": "й_View item details__________________й",
+    "hideItem": "й_Hide item details__________________й",
+    "created": "й_Created_______________й",
+    "updated": "й_Updated_______________й",
+    "viewCount": "й_View Count_____________________й",
+    "rating": "й_Rating_____________й"
+  },
+  "pager": {
+    "previous": "й_Previous_________________й",
+    "next": "й_Next_________й",
+    "paginationLimit": "й_There is a limit of 10,000 results that can be retrieved via pagination. Try filtering or changing your search term to lower the number of returned results_______________________________________________________________________________й."
+  },
+  "actions": {
+    "viewMap": "й_View Map_________________й",
+    "viewScene": "й_View Scene_____________________й",
+    "viewApp": "й_View App_________________й",
+    "viewInMap": "й_View in Map____________й",
+    "viewInScene": "й_View in Scene______________й",
+    "view": "й_View_________й",
+    "download": "й_Download_________________й",
+    "openPdf": "й_Open in PDF____________й",
+    "open": "й_Open_________й",
+    "openDoc": "й_Open Document______________й",
+    "openTab": "й_Open in new tab________________й",
+    "openViewer": "й_Open in Map Viewer___________________й",
+    "addFavorite": "й_Add to Favorites_________________й",
+    "removeFavorite": "й_Remove from Favorites______________________й"
+  },
+  "viewer": {
+    "back": "й_Return to Gallery__________________й",
+    "viewLoading": {
+      "scripts": "й_Fetching scripts___________________й..",
+      "map": "й_Preparing the map____________________й..",
+      "basemap": "й_Loading the basemap______________________й..",
+      "layers": "й_Processing layers____________________й..",
+      "view": "й_Initializing the view________________________й..",
+      "widgets": "й_Adding Widgets_________________й..",
+      "failed": "й_The item failed to load_________________________й.",
+      "sorry": "й_Sorry, something went wrong loading the requested item_____________________________й."
+    }
+  },
+  "share": {
+    "shareGallery": "й_Share gallery______________й",
+    "shareFiltered": "й_Share filtered gallery_______________________й",
+    "tooltip": "й_URL copied to you clipboard____________________________й"
+  }
+});
